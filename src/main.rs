e05f72c2a7c918fa20636254b6b4dfa09efdb215
Adegbite Paul <105382756+octopuspaul110@users.noreply.github.com>
2026-03-19 15:53:16 +0100

    hello world server returner...

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+#![allow(unused)]
+
+use std::net::SocketAddr;
+use axum::{Router, response::Html, routing::get};
+
+#[tokio::main]
+async fn main() {
+    let routes_hello = Router::new().route(
+    "/hello",
+    get(|| async {
+        Html("Hello <strong>World!!!</strong>")
+    }),
+    );
+
+    // start server
+    let addr = SocketAddr::from(([127,0,0,1],8080));
+    println!("->> Listening on {addr}\n");
+
+    axum::Server::bind(&addr)
+    .serve(routes_hello.into_make_service())
+    .await
+    .unwrap();
 }
